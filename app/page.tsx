00d19aa2c0d56b210b5edd275dbd1fe107746301
jawadifti <jawadijaj@gmail.com>
2024-10-08 19:55:14 +0200

    bug resolved

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Welcome to Jawad company, we will help you build technology
+            Welcome to Jawad company, we will help you build technology. All bugs are resolved.
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               app/page.tsx
             </code>
